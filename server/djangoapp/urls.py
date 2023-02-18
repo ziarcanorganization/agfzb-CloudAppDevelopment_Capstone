899ce81c78c1515b35ebc399b04713cecc5138ca
Branislav Balaz <branislav.balaz@xit.camp>
2023-02-18 13:24:59 +0100

    static page finished (week1 static pages)

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     # name the URL
 
     # path for about view
-
+    path('about/', views.about, name='about'),
     # path for contact us view
-
+    path('contact/', views.contact, name='contact'),
     # path for registration
 
     # path for login
