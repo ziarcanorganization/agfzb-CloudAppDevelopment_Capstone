7984f8c1edb544a0b297507938ab1c88c7003a29
ziarcan <ziarcan@gmail.com>
2023-02-24 11:12:07 -0500

    finish registration page

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,11 +14,11 @@ urlpatterns = [
     # path for contact us view
     path('contact/', views.contact, name='contact'),
     # path for registration
-
+    path('registration/', views.registration_request, name='registration'),
     # path for login
-
+    path('login/', views.login_request, name='login'),
     # path for logout
-
+    path('logout/', views.logout_request, name='logout'),
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
