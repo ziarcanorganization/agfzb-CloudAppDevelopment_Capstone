ae9865727f29afea3fa0ed0a7874bd3369e6bb35
Branislav Balaz <branislav.balaz@xit.camp>
2023-04-02 16:43:14 +0200

    get_dealer_by_id_from_cf implemented

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -63,6 +63,20 @@ def get_dealers_from_cf(url, **kwargs):
 # def get_dealer_by_id_from_cf(url, dealerId):
 # - Call get_request() with specified arguments
 # - Parse JSON results into a DealerView object list
+def get_dealer_by_id_from_cf(url, id):
+    json_result = get_request(url, id=id)
+    print('json_result from line 54',json_result)
+
+    if json_result:
+        dealers = json_result
+
+
+        dealer_doc = dealers[0]
+        dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"],
+                               id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"], full_name=dealer_doc["full_name"],
+
+                               short_name=dealer_doc["short_name"], st=dealer_doc["st"], zip=dealer_doc["zip"])
+    return dealer_obj
 
 
 # Create an `analyze_review_sentiments` method to call Watson NLU and analyze text
